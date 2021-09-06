fe7bbe96af2542178dc8f3a076fc08c95d16946f
ashleyannlaz <paperpastels@gmail.com>
2021-09-06 19:03:17 -0400

    Finished guided

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -1,3 +1,4 @@
+const db = require('../../data/db-config')
 module.exports = {
   get,
   getById,
@@ -7,21 +8,30 @@ module.exports = {
 }
 
 async function get() {
-  return 'get wired'
+  // select * from posts
+  return db('posts');
 }
 
-async function getById() {
-  return 'getById wired'
+async function getById(id) {
+  // return db() where({id:id, foo: 'bar})
+  // without .first() we will always get an array
+  return db('posts').where('id', id).first()
 }
 
-async function create() {
-  return 'create wired'
+async function create({ title, contents }) {
+  const [id] = await db('posts').insert({ title, contents });
+  const newPost = await getById(id);
+  return newPost
+  
 }
 
-async function update() {
-  return 'update wired'
+async function update(id, { title, contents}) {
+  return db('posts').where('id', id).update({ title, contents})
+  .then(() => {
+    return getById(id)
+  })
 }
 
-async function remove() {
-  return 'delete wired'
+async function remove(id) {
+  return db('posts').where('id', id).del()
 }
